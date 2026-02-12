7bcd375a5bac91fd0769f1d0c42b66e90eec1691
Oscar Fernandez <ojcarfr@gmail.com>
2026-02-12 10:51:13 +0100

    Creation of unit type

---
+++ src/Rufus/_.cs
@@ -0,0 +1,32 @@
+﻿namespace Rufus;
+
+using System.Diagnostics.CodeAnalysis;
+
+/// <summary>
+///     Unit value that returns same input to any pure function.
+///     It uses '_' as name in order to use same disregard that variables when declaring generic types.
+/// </summary>
+/// <remarks>
+///     This type is a value type that always is equals to any unit value else.
+/// </remarks>
+#pragma warning disable CA2231
+public readonly struct _ : IEquatable<_>
+#pragma warning restore CA2231
+{
+    /// <inheritdoc />
+    public bool Equals(_ other) => true;
+
+    /// <inheritdoc />
+    public override bool Equals([NotNullWhen(true)] object? obj) => obj is _;
+
+    /// <inheritdoc />
+    public override int GetHashCode() => 0;
+
+    /// <inheritdoc />
+    public override string ToString()
+    {
+        const string VALUE = "()";
+
+        return VALUE;
+    }
+}
